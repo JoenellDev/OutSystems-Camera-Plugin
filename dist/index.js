35f02b2fadf970279564ea9a3bdf4145708bb777
Joenell Dycausing <joenelldycausing623@gmail.com>
2026-09-20 09:59:51 +0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CameraPlugin = void 0;
-class CameraPlugin {
-    async takePhoto(options) {
-        throw new Error("Native camera implementation not connected yet.");
-    }
-}
-exports.CameraPlugin = CameraPlugin;
